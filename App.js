917989ff5715c3a3e1d77d6f4d1915bb03b0d755
Amna Subic <amna.subic@stu.ssst.edu.ba>
2020-06-02 15:46:42 +0200

    /

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import StudentTabNavigation from './routes/StudentTabNavigation';
 import { NavigationContainer } from '@react-navigation/native';
-import {LandingPage} from './screens/LandingPage';
+//import {LandingPage} from './screens/LandingPage';
 
 import WelcomePage from './screens/WelcomeScreen.js';
 
 
 export default function App(){
-    const isLoggedIn = true;
+    const isLoggedIn = false;
     return (
             <NavigationContainer>
-               {isLoggedIn ? <StudentTabNavigation/> : LandingPage()}
+               {isLoggedIn ? <StudentTabNavigation/> : WelcomePage()}
             </NavigationContainer>
       );
 }
